0e175990bd2f98b6ce773ef30dfc4a8a12a086b8
Mohamed Hussein <mohamed.hussein@smicolon.com>
2025-12-14 05:32:26 +0100

    Fix SwitchProps type conflict with HTML size attribute
    Omit 'size' from InputHTMLAttributes to prevent type conflict
    between the HTML size attribute (number) and our component's
    size prop (sm | default | lg).

--- packages/smi-ui/registry/ui/switch/switch.tsx
+++ packages/smi-ui/registry/ui/switch/switch.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { cn } from "../../../src/lib/utils"
 
 export interface SwitchProps
-  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "type"> {
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "type" | "size"> {
   /** Label text */
   label?: string
   /** Description text */
